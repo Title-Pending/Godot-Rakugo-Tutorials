30fdb6367a4f5d74009762a266435dbfb0673d4b
Zon Petilla <Burningwoodmedia@gmail.com>
2019-09-11 02:47:10 +0900

    started animating the cube
    Okay I got an animation to play within another animation

--- Godot Level 0/Lesson 2 Variables and Logic Operators/Assets/Levels/SwitchTestLevel.gd
+++ Godot Level 0/Lesson 2 Variables and Logic Operators/Assets/Levels/SwitchTestLevel.gd
@@ -11,17 +11,25 @@ var c = false
 func _process(delta):
 	if Input.is_action_just_pressed("switch_a"): 
 		a = !a
+		$Switch/SpotLight.visible = !$Switch/SpotLight.visible
 		anim($Switch/CSGMesh/AnimationPlayer, a)
+		$CSGMesh_Cube/BlockAni.play ("switch_a")
+
+
 		return
 
 	if Input.is_action_just_pressed("switch_b"): 
 		b = !b
+		$Switch2/SpotLight2.visible = !$Switch2/SpotLight2.visible
 		anim($Switch2/CSGMesh/AnimationPlayer, b)
+		$CSGMesh_Cube/BlockAni.play ("switch_b")
 		return
 		
 	if Input.is_action_just_pressed("switch_c"): 
 		c = !c
+		$Switch3/SpotLight3.visible = !$Switch3/SpotLight3.visible
 		anim($Switch3/CSGMesh/AnimationPlayer, c)
+		$CSGMesh_Cube/BlockAniC.play ("Move")
 		return
 	
 func anim(anim, value):
